a734de3d2523e825cfe5ec355fb5b28c0fe9c201
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-04 12:45:19 +0200

    fix: clamp status text to available width

--- screen.lua
+++ screen.lua
@@ -100,6 +100,8 @@ function ThermoSudokuScreen:buildLayout()
         or  math.floor(sw * 0.9)
     local keypad_width = is_landscape and button_width or math.floor(sw * 0.75)
 
+    self.status_text:setMaxWidth(is_landscape and button_width or board_frame_size)
+
     local title_bar = self:buildTitleBar(_("Thermo Sudoku"), function()
         return {
             { text = _("New game"),                  callback = function() self:onNewGame() end },
