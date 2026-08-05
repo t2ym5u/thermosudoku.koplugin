97d88eb0c079e1d1a3a5dae23491caec5d953c76
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 15:07:48 +0200

    chore: bump version to 1.2.15

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Thermo Sudoku"),
     description = _("Sudoku with thermometer constraints"),
-    version     = "1.2.14",
+    version     = "1.2.15",
 }
